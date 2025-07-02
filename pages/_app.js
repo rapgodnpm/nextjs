ef5a469b6f327cbe95214cd000fca33f282a60bb
rapgodnpm <54170629+rapgodnpm@users.noreply.github.com>
2025-07-02 14:00:21 +0000

    da

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  for(let i = 0; i < 100; i++){
+    console.log('da')
+  }
+
   return <Component {...pageProps} />
 }
 
